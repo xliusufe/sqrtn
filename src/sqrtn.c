39c825b63fcfe5b4b8c481ac0d2f4eddd2833a48
XLiu <liu.xu@sufe.edu.cn>
2019-03-29 18:25:15 +0800

    updated

--- src/sqrtn.c
+++ src/sqrtn.c
@@ -3,193 +3,140 @@
 #include <R.h>
 #include <Rinternals.h>  // required for SEXP et.al.;
 #include <string.h> // required for memcpy()
-
+#define min(a,b) ((a > b) ? b : a)
 #define max(a,b) ((a > b) ? a : b)
 
-int plus(int *a, int *b, int curr_len){
-	//add b to a
-	int i;
-	int temp,temp1=0;
-	for(i=0;i<curr_len;i++){
-		temp = a[i]+b[i]+temp1; 
-		a[i]=temp%10;
-		temp1 = temp/10;
-	}
-	if(temp1>0) a[curr_len++]=temp1;
-	return curr_len;
-}
-
-int compare0(int *a, int *b, int La){
-	int i;	
-	for(i=La-1;i>=0;i--){
-		if(a[i]>b[i]) return 1;
-		if(a[i]<b[i]) return 0;	
-	}
-	return 1;
-}
-
-void minus0(int *a, int *b, int Lb){
-	int i,temp;
-	for(i=0;i<Lb;i++){
-		temp=a[i]-b[i]; 
-		temp>=0?(a[i]=temp):(a[i]=temp+10,a[i+1]--);	
-	}
-}
-
-int product_single(int *b, int curr_len, int n){
-	int i,temp,temp1=0,flag;
-	for(i=0;i<curr_len;i++){
-		temp = b[i]*n+temp1; 
-		b[i]=temp%10;
-		temp1 = temp/10;
-	}
-	if(temp1>0){ b[curr_len]=temp1;flag=1;}
-	else flag=0;
-	return flag;
-}
-
-int pell(int *a, int *b, int *a1, int *b1, int *Lab, int N, int L0){
-	int i,n=14,La,Lb,flaga,flagb;
+int pell(int *a1, int *b1, int *Lab, int N, int L0){
+	int i,n=14,La,ta,tb,ta1,tb1;
+	int *a,*b;
 	
-	a[0]=7;b[0]=5;
-	a[1]=0;b[1]=4;
-	a[2]=0;b[2]=0;
-	a[3]=6;b[3]=0;
-	a[4]=8;b[4]=7;
-	a[5]=1;b[5]=3;
-	a[6]=2;b[6]=5;
-	a[7]=1;b[7]=4;
-	a[8]=8;b[8]=6;
-	a[9]=0;b[9]=7;
-	a[10]=1;
+	a1[0]=7;b1[0]=5;	a1[1]=0;b1[1]=4;
+	a1[2]=0;b1[2]=0;	a1[3]=6;b1[3]=0;
+	a1[4]=8;b1[4]=7;	a1[5]=1;b1[5]=3;
+	a1[6]=2;b1[6]=5;	a1[7]=1;b1[7]=4;
+	a1[8]=8;b1[8]=6;	a1[9]=0;b1[9]=7;
+	a1[10]=1;
 
 	Lab[0]=11;Lab[1]=10;
-	for(i=0;i<Lab[0];i++)a1[i]=a[i];
-    for(i=0;i<Lab[1];i++)b1[i]=b[i];
-	Lab[2]=n;
+	Lab[2]=n;	
 	while(n<N){
-		La = plus(a,b1,max(Lab[0],Lab[1]));
-		flaga=product_single(a1,Lab[0],2);
-		flagb=product_single(b1,Lab[1],3);
-		
-		La = plus(a,a1,max(La,Lab[0]+flaga));
-		La = plus(a,b1,max(La,Lab[1]+flagb));			
-
-		Lb = Lab[1]+flagb;
-		for(i=0;i<Lb;i++) b[i]=b1[i];
-		Lb = max(Lb,Lab[0]+flaga);		
-		Lb = plus(b,a1,Lb);
-
-		Lab[0]=La; Lab[1]=Lb; Lab[2]+=1;
-		if(Lab[1]>=L0) return 1;
-		n++;
-
-		for(i=0;i<La;i++)a1[i]=a[i];
-		for(i=0;i<Lb;i++)b1[i]=b[i];
-	}	
+		ta=tb=0;a=&a1[0];b=&b1[0];
+		La=Lab[0]+1;
+		for(i=0;i<La;i++){
+			ta1=(*a)*17+(*b)*24+ta;
+			tb1=(*a)*12+(*b)*17+tb;
+			*a=ta1%10;
+			*b=tb1%10;
+			ta=ta1/10;
+			tb=tb1/10;	
+			a++;b++;
+		}
+		Lab[0]=La;Lab[1]=La;
+		if(ta){*a+=ta;Lab[0]++;}
+		if(tb){*b+=tb;Lab[1]++;}
+		if(Lab[1]+1>=L0) return 1;
+		n+=2;
+	}		
 	return 0;
 }
 
-int divide_k(int *a, int *b,int *bcopy, int La, int Lb, int temp1){
-	int flag;
-
-	if(temp1==1) minus0(a,bcopy,Lb);
-	else{		
-		flag=product_single(bcopy,Lb,temp1);
-		Lb+=flag;
-		if(compare0(a,bcopy,La))
-			minus0(a,bcopy,Lb);
-		else{
-			minus0(bcopy,b,Lb);
-			minus0(a,bcopy,Lb);			
-			temp1--;
-		}			
+void plus1(int *a, int b, int curr_len){
+	int *pa=&a[curr_len-1];
+	for(int i=0;i<curr_len;i++,pa--){
+		b+=*pa;	*pa=b%10; b = b/10;
+		if(!b) break;
 	}
-	return temp1;
 }
 
-int divide_k0(int *a, int *b,int *bcopy, int La, int Lb, int temp1){
-	int flag;
-
-	if(temp1==0) return 0;
-	else if(temp1==1){
-		if(!compare0(a,bcopy,La)) return 0;
-		minus0(a,bcopy,Lb);
+void minus0(int *a, int *b, int *pa2, int Lb){
+	int i,temp,*pa,*pb=&b[0];
+	pa=pa2;
+	for(i=0;i<Lb;i++){
+		temp=*pa-*pb; 
+		if(temp>=0){*pa=temp;pa++;}
+		else{*pa=temp+10;pa++;(*pa)--;}
+		pb++;
 	}
-	else{		
-		flag=product_single(bcopy,Lb,temp1);
-		if(flag){		
-			minus0(bcopy,b,Lb);
-			minus0(a,bcopy,Lb);	
-			temp1--;
-		}
-		else{
-			if(compare0(a,bcopy,La))
-				minus0(a,bcopy,Lb);
-			else{
-				minus0(bcopy,b,Lb);
-				minus0(a,bcopy,Lb);			
-				temp1--;
-			}		
-		}		
-	}
-	return temp1;
 }
 
-int divide_single(int *a, int *b,int *bcopy, int La, int Lb, int digit){
-	int i,temp,temp1,tempb;	
-	for(i=0;i<Lb;i++) bcopy[i]=b[i];bcopy[Lb]=0;
+void divide_k(int *a, int *b, int La, int *pa2, int n){
+	int i,*pa,*pb=&b[0];
+	pa=pa2+La-1;
+	pa=pa2;
+	int temp,temp1=0;
+	for(i=0;i<La;i++){
+		temp1=(*pb)*n+temp1/10;
+		temp=*pa-temp1%10; 
+		if(temp>=0){*pa=temp;pa++;}
+		else{*pa=temp+10;pa++;(*pa)--;}
+		pb++;
+	}
+	if(temp1/10)	*pa-=*pb;
+	pa=pa2+La-1;
+}
 
-	if(La>Lb){
-		temp=tempb=0;temp1=1;
-		for(i=digit;i>0;i--){
-			temp+=a[Lb+1-i]*temp1;
+int divide_single(int *a, int *b, int La, int *pa1,int *pa2, int Lb, int digit,int tempb){
+	int i,temp,temp1,*pa;	
+	if(La>Lb){	
+		pa=pa1-digit;
+		temp=0;temp1=1;
+		for(i=0;i<digit+1;i++,pa++){
+			temp+=(*pa)*temp1;
 			temp1*=10;
 		}
-		temp1=1;
-		for(i=digit-1;i>0;i--){
-			tempb+=b[Lb-i]*temp1;
-			temp1*=10;
+		temp1=temp/tempb;
+		if(temp1==0) return 0;
+		if(temp1==1) minus0(a,b,pa2,Lb);
+		else{
+			temp1 = min(temp1,temp/(tempb+1));
+			if(temp1==1) minus0(a,b,pa2,Lb);
+			else divide_k(a,b,La,pa2,temp1);
 		}
-		temp1 = temp/tempb;
-		temp1 = divide_k(a,b,bcopy,La,Lb,temp1);		
 	}
 	else{
-		temp=tempb=0;temp1=1;
-		for(i=digit;i>0;i--){
-			temp+=a[Lb-i]*temp1;
-			tempb+=b[Lb-i]*temp1;
+		pa=pa1-digit+1;
+		temp=0;temp1=1;
+		for(i=0;i<digit;i++,pa++){
+			temp+=(*pa)*temp1;
 			temp1*=10;
 		}
-		temp1 = temp/tempb;
-		temp1 = divide_k0(a,b,bcopy,La,Lb,temp1);	
+		temp1=temp/tempb;
+		if(temp1==0) return 0;
+		if(temp1==1) minus0(a,b,pa2,Lb);
+		else{
+			temp1 = min(temp1,temp/(tempb+1));
+			if(temp1==1) minus0(a,b,pa2,Lb);
+			else divide_k(a,b,La,pa2,temp1);
+		}
 	}
 	return temp1;	
 }
 
 void divide(int *quotient, int *a, int *b, int La, int Lb,int prec){
-	int i,pr=0,count,Lb1=Lb+1,*bcopy,digit;
-	bcopy=(int *)malloc(sizeof(int)*Lb1);
+	int i,pr=0,count,digit,temp1,tempb=0,*pa1,*pa2;
 	if(Lb<8) digit=3;
 	else digit=4;
-
-	minus0(a,b,Lb);
+	temp1=1;
+	for(i=digit;i>0;i--){
+		tempb+=b[Lb-i]*temp1;
+		temp1*=10;
+	}
+	pa2=&a[2*prec-La]; 
+	minus0(a,b,pa2,Lb);
+	pa1=&a[2*prec-1];
 	while(pr<prec){
+		pa1=pa2+Lb-1;
 		count=0;	
-		while(!a[Lb-1-count]) count++;
+		while(!(*pa1)){count++;pa1--;}
 		if(count){ 	
-			if(count>1)	pr+=count-1;			
-			for(i=Lb;i>=count;i--)a[i]=a[i-count];
-			for(i=0;i<count;i++)a[i]=0;			
-			La=Lb;
+			if(count>1)	pr+=count-1;						
+			La=Lb; pa2-=count; 
 		}
-		else{
-			La=Lb+1;
-			for(i=Lb;i>0;i--)a[i]=a[i-1]; a[0]=0; 			
-		}
-		quotient[pr++] = divide_single(a,b,bcopy,La,Lb,digit);	
+		else{La=Lb+1; pa2--;}
+		temp1 = divide_single(a,b,La,pa1,pa2,Lb,digit,tempb);	
+		if(temp1<10) quotient[pr++]=temp1;
+		else plus1(quotient,temp1,++pr);
 	}	
-	free(bcopy);
 }
 
 void precesion(char *quotient_s, int prec){	
@@ -208,17 +155,15 @@ void precesion(char *quotient_s, int prec){
 	else{
 		quotient=(int *)malloc(sizeof(int)*prec);
 		for(i=0;i<prec;i++) quotient[i]=0;
-		a=(int *)malloc(sizeof(int)*prec);
+		a=(int *)malloc(sizeof(int)*2*prec);
 		b=(int *)malloc(sizeof(int)*prec);
-		int *a1=(int *)malloc(sizeof(int)*prec);
-		int *b1=(int *)malloc(sizeof(int)*prec);
 		int *Lab=(int *)malloc(sizeof(int)*3);
 		Lab[0]=Lab[1]=Lab[2]=0;
-		for(i=0;i<prec;i++) a1[i]=b1[i]=a[i]=b[i]=0;
+		for(i=0;i<prec;i++) a[i]=b[i]=0;
 
-		pell(a,b,a1,b1,Lab,prec,prec/2);
+		pell(a,b,Lab,prec,prec/2);
 		divide(quotient,a,b,Lab[0],Lab[1],prec);
-		free(a1);free(b1);free(Lab);
+		free(Lab);
 	}
 	quotient_s[0]='1';quotient_s[1]='.';
 	for(i=2;i<prec+2;i++) quotient_s[i] = quotient[i-2]+48;
