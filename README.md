92691dc034a10d6ae746ab89af9374aaa43158db
XLiu <liu.xu@sufe.edu.cn>
2019-03-30 10:30:00 +0800

    updated

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # sqrtn
-An R pacakge to calculate sqrt(n) with very high precision. Currenly, we approximate sqrt(n) with n<10, that is, sqrt(2), sqrt(3), sqrt(5), sqrt(6), sqrt(7), and sqrt(8) only. "sqrtn"" implements dramatically fast. It takes only less than 1 minute to approximate sqrt(2) with 100,000 digits.
+An R pacakge to calculate sqrt(n) with very high precision. Currenly, we approximate sqrt(n) with n<10, that is, sqrt(2), sqrt(3), sqrt(5), sqrt(6), sqrt(7), and sqrt(8) only. "sqrtn"" implements dramatically fast. It takes only less than 30 seconds to approximate sqrt(2) with 100,000 digits.
 
 # Installation
 
@@ -11,7 +11,7 @@ An R pacakge to calculate sqrt(n) with very high precision. Currenly, we approxi
 
    - [x] [sqrtn-manual.pdf](https://github.com/xliusufe/sqrtn/blob/master/inst/sqrtn-manual.pdf) ---------- Details of the usage of the package.
    
-   - [x] [sqrt2-approx.pdf](https://github.com/xliusufe/sqrtn/blob/master/inst/sqrt2-approx.pdf) ---------- The first one million digits of the square root of 2.
+   - [x] [sqrt2.pdf](https://github.com/xliusufe/sqrtn/blob/master/inst/sqrt2-approx.pdf) ---------- The first one million digits of the square root of 2.
    
    - [x] [sqrt3.md](https://github.com/xliusufe/sqrtn/blob/master/inst/sqrt3.md) ---------- The first many digits of the square root of 3.
       
--- src/sqrtn.c
+++ src/sqrtn.c
@@ -1,12 +1,13 @@
-#include <stdio.h>  // required for exit
-#include <stdlib.h> // required for malloc(), free();
+#include <stdio.h>  
+#include <stdlib.h> 
+#include <math.h>
 #include <R.h>
-#include <Rinternals.h>  // required for SEXP et.al.;
-#include <string.h> // required for memcpy()
+#include <Rinternals.h> 
+#include <string.h> 
 #define min(a,b) ((a > b) ? b : a)
 #define max(a,b) ((a > b) ? a : b)
 
-int pell2(int *a1, int *b1, int *Lab, int N, int L0){
+int pf2(int *a1, int *b1, int *Lab, int N, int L0){
 	int i,n=14,La,ta,tb,ta1,tb1,*a,*b;
 	a1[0]=7;b1[0]=5; a1[1]=0;b1[1]=4; a1[2]=0;b1[2]=0;	a1[3]=6;b1[3]=0;
 	a1[4]=8;b1[4]=7; a1[5]=1;b1[5]=3; a1[6]=2;b1[6]=5; a1[7]=1;b1[7]=4;
@@ -31,7 +32,7 @@ int pell2(int *a1, int *b1, int *Lab, int N, int L0){
 	return 0;
 }
 
-int pell3(int *a1, int *b1, int *Lab, int N, int L0){
+int pf3(int *a1, int *b1, int *Lab, int N, int L0){
 	int i,n=8,La,ta,tb,ta1,tb1,*a,*b;
 	a1[0]=1;b1[0]=0; a1[1]=5;b1[1]=4;	a1[2]=1;b1[2]=3; a1[3]=2;b1[3]=6;
 	a1[4]=8;b1[4]=2; a1[5]=3;b1[5]=6;	a1[6]=3;b1[6]=4; a1[7]=6;b1[7]=9;
@@ -56,7 +57,7 @@ int pell3(int *a1, int *b1, int *Lab, int N, int L0){
 	return 0;
 }
 
-int pell5(int *a1, int *b1, int *Lab, int N, int L0){
+int pf5(int *a1, int *b1, int *Lab, int N, int L0){
 	int i,n=10,La,ta,tb,ta1,tb1,*a,*b;	
 	a1[0]=9;b1[0]=6; a1[1]=0;b1[1]=3; a1[2]=8;b1[2]=6; a1[3]=6;b1[3]=5;
 	a1[4]=7;b1[4]=8; a1[5]=0;b1[5]=7; a1[6]=0;b1[6]=3; a1[7]=5;b1[7]=3;
@@ -81,7 +82,32 @@ int pell5(int *a1, int *b1, int *Lab, int N, int L0){
 	return 0;
 }
 
-int pell7(int *a1, int *b1, int *Lab, int N, int L0){
+int pf6(int *a1, int *b1, int *Lab, int N, int L0){
+	int i,n=10,La,ta,tb,ta1,tb1,*a,*b;	
+	a1[0]=5;b1[0]=8; a1[1]=4;b1[1]=9; a1[2]=4;b1[2]=9; a1[3]=7;b1[3]=2;
+	a1[4]=7;b1[4]=0; a1[5]=1;b1[5]=3; a1[6]=6;b1[6]=5; a1[7]=1;b1[7]=5;
+	a1[8]=7;b1[8]=2; a1[9]=4;b1[9]=8; a1[10]=4;b1[10]=1;
+	Lab[0]=11;Lab[1]=11;Lab[2]=n;	
+	while(n<N){
+		ta=tb=0;a=&a1[0];b=&b1[0];
+		La=Lab[0]+2;
+		for(i=0;i<La;i++){
+			ta1=(*a)*49+(*b)*120+ta;
+			tb1=(*a)*20+(*b)*49+tb;
+			*a=ta1%10;*b=tb1%10;
+			ta=ta1/10;tb=tb1/10;	
+			a++;b++;
+		}
+		Lab[0]=La;Lab[1]=La;
+		if(ta){*a+=ta;Lab[0]++;}
+		if(tb){*b+=tb;Lab[1]++;}
+		if(Lab[1]+1>=L0) return 1;
+		n+=2;
+	}		
+	return 0;
+}
+
+int pf7(int *a1, int *b1, int *Lab, int N, int L0){
 	int i,n=10,La,ta,tb,ta1,tb1,*a,*b;	
 	b1[0]=5;a1[0]=4; b1[1]=9;a1[1]=2; b1[2]=7;a1[2]=2; b1[3]=1;a1[3]=3;
 	b1[4]=2;a1[4]=7; b1[5]=5;a1[5]=8; b1[6]=5;a1[6]=6; b1[7]=3;a1[7]=5;
@@ -106,6 +132,29 @@ int pell7(int *a1, int *b1, int *Lab, int N, int L0){
 	return 0;
 }
 
+int pf8(int *a1, int *b1, int *Lab, int N, int L0){
+	int i,n=10,La,ta,tb,ta1,tb1,*a,*b;	
+	a1[0]=3;b1[0]=1; a1[1]=8;b1[1]=8; a1[2]=0;b1[2]=9; a1[3]=4;b1[3]=7;
+	a1[4]=5;b1[4]=0; a1[5]=5;b1[5]=4; a1[6]=8;b1[6]=3; a1[7]=7;b1[7]=8;
+	a1[8]=4;b1[8]=5; a1[9]=4;b1[9]=1; Lab[0]=10;Lab[1]=10;Lab[2]=n;	
+	while(n<N){
+		ta=tb=0;a=&a1[0];b=&b1[0];
+		La=Lab[0]+2;
+		for(i=0;i<La;i++){
+			ta1=(*a)*99+(*b)*280+ta;
+			tb1=(*a)*35+(*b)*99+tb;
+			*a=ta1%10;*b=tb1%10;
+			ta=ta1/10;tb=tb1/10;	
+			a++;b++;
+		}
+		Lab[0]=La;Lab[1]=La;
+		if(ta){*a+=ta;Lab[0]++;}
+		if(tb){*b+=tb;Lab[1]++;}
+		if(Lab[1]+1>=L0) return 1;
+		n+=2;
+	}		
+	return 0;
+}
 
 void plus1(int *a, int b, int curr_len){
 	int *pa=&a[curr_len-1];
@@ -203,70 +252,27 @@ void divide(int *quotient, int *a, int *b, int La, int Lb,int prec){
 }
 
 void precesion(char *quotient_s, int prec,int n){	
-	int i,*a,*b,*quotient;
-	if(prec<22){
-		quotient=(int *)malloc(sizeof(int)*22);
-		a=(int *)malloc(sizeof(int)*21);
-		b=(int *)malloc(sizeof(int)*21);
-		if(n==2){
-			quotient[0]=4;quotient[1]=1;quotient[2]=4;quotient[3]=2;
-			quotient[4]=1;quotient[5]=3;quotient[6]=5;quotient[7]=6;
-			quotient[8]=2;quotient[9]=3;quotient[10]=7;quotient[11]=3;
-			quotient[12]=0;quotient[13]=9;quotient[14]=5;quotient[15]=0;
-			quotient[16]=4;quotient[17]=8;quotient[18]=8;quotient[19]=0;
-			quotient[20]=1;
-		}
-		if(n==3){
-			quotient[0]=7;quotient[1]=3;quotient[2]=2;quotient[3]=0;
-			quotient[4]=5;quotient[5]=0;quotient[6]=8;quotient[7]=0;
-			quotient[8]=7;quotient[9]=5;quotient[10]=6;quotient[11]=8;
-			quotient[12]=8;quotient[13]=7;quotient[14]=7;quotient[15]=2;
-			quotient[16]=9;quotient[17]=3;quotient[18]=5;quotient[19]=2;
-			quotient[20]=7;		
-		}
-		if(n==5){
-			quotient[0]=2;quotient[1]=3;quotient[2]=6;quotient[3]=0;
-			quotient[4]=6;quotient[5]=7;quotient[6]=9;quotient[7]=7;
-			quotient[8]=7;quotient[9]=4;quotient[10]=9;quotient[11]=9;
-			quotient[12]=7;quotient[13]=8;quotient[14]=9;quotient[15]=6;
-			quotient[16]=9;quotient[17]=6;quotient[18]=4;quotient[19]=0;
-			quotient[20]=9;	
-		}
-		if(n==7){
-			quotient[0]=6;quotient[1]=4;quotient[2]=5;quotient[3]=7;
-			quotient[4]=5;quotient[5]=1;quotient[6]=3;quotient[7]=1;
-			quotient[8]=1;quotient[9]=0;quotient[10]=6;quotient[11]=4;
-			quotient[12]=5;quotient[13]=9;quotient[14]=0;quotient[15]=5;
-			quotient[16]=1;quotient[17]=6;quotient[18]=1;quotient[19]=5;
-			quotient[20]=7;			
-		}
-	}  		
-	else{
-		quotient=(int *)malloc(sizeof(int)*prec);
-		for(i=0;i<prec;i++) quotient[i]=0;
-		a=(int *)malloc(sizeof(int)*2*prec);
-		b=(int *)malloc(sizeof(int)*prec);
-		int *Lab=(int *)malloc(sizeof(int)*3);
-		Lab[0]=Lab[1]=Lab[2]=0;
-		for(i=0;i<prec;i++) b[i]=0;
-		for(i=0;i<2*prec;i++) a[i]=0;
-		if(n==2) pell2(a,b,Lab,prec,prec/2);
-		if(n==3) pell3(a,b,Lab,prec,prec/2);
-		if(n==5) pell5(a,b,Lab,prec,prec/2);
-		if(n==7) pell7(a,b,Lab,prec,prec/2);
-		int *pa1=&a[2*prec-1],*pa2=&a[Lab[0]-1];
-		for(i=0;i<Lab[0];i++,pa1--,pa2--){*pa1=*pa2;*pa2=0;}
-		divide(quotient,a,b,Lab[0],Lab[1],prec);
-		free(Lab);
-	}
-	if(n==2)quotient_s[0]='1';
-	if(n==3)quotient_s[0]='1';
-	if(n==5)quotient_s[0]='2';
-	if(n==7)quotient_s[0]='2';
-	quotient_s[1]='.';
+	int i,*a,*b,*quotient,f1=sqrt(1.0*n); 	
+	quotient=(int *)malloc(sizeof(int)*prec);
+	for(i=0;i<prec;i++) quotient[i]=0;
+	a=(int *)malloc(sizeof(int)*2*prec);
+	b=(int *)malloc(sizeof(int)*prec);
+	int *Lab=(int *)malloc(sizeof(int)*3);
+	for(i=0;i<prec;i++) b[i]=0;
+	for(i=0;i<2*prec;i++) a[i]=0;
+	if(n==2) pf2(a,b,Lab,prec,prec/2);
+	if(n==3) pf3(a,b,Lab,prec,prec/2);
+	if(n==5) pf5(a,b,Lab,prec,prec/2);
+	if(n==6) pf5(a,b,Lab,prec,prec/2);
+	if(n==7) pf7(a,b,Lab,prec,prec/2);
+	if(n==8) pf8(a,b,Lab,prec,prec/2);
+	int *pa1=&a[2*prec-1],*pa2=&a[Lab[0]-1];
+	for(i=0;i<Lab[0];i++,pa1--,pa2--){*pa1=*pa2;*pa2=0;}
+	divide(quotient,a,b,Lab[0],Lab[1],prec);
+	quotient_s[0]=f1+48; quotient_s[1]='.';
 	for(i=2;i<prec+2;i++) quotient_s[i] = quotient[i-2]+48;
 	quotient_s[prec+2]='\0';
-	free(a);free(b);free(quotient);
+	free(a);free(b);free(quotient);free(Lab);
 }
 
 SEXP sqrtn(SEXP prec0, SEXP N)
